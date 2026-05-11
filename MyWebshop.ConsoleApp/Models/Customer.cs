c5720d7e80c79b556794b7517ad98a120a482ab2
Kees van Loenen <kees.vanloenen@infosupport.com>
2026-05-11 13:58:02 +0200

    Data annotations explanation

--- MyWebshop.ConsoleApp/Models/Customer.cs
+++ MyWebshop.ConsoleApp/Models/Customer.cs
@@ -1,7 +1,26 @@
-﻿namespace MyWebshop.ConsoleApp.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyWebshop.ConsoleApp.Models;
 
 public class Customer
 {
+    [Key]
     public int Id { get; set; }
+
+    //[Column(TypeName= "uniqueidentifier")]
+    //public Guid Id { get; set; } = Guid.NewGuid();
+
+    [Required]
+    [MaxLength(100)]
     public required string Name { get; set; }
+
+    [MaxLength(20)]
+    public string? PhoneNumber { get; set; }
+
+    [Column(TypeName = "decimal(8,2)")]
+    public decimal CreditLimit { get; set; }
+
+    [Column(TypeName = "datetime2(0)")]
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
