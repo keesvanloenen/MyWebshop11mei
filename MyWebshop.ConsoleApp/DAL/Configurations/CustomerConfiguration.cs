ff6cc2f9082eb29d3a2198624fd22afbfb513c77
Kees van Loenen <kees.vanloenen@infosupport.com>
2026-05-11 14:35:53 +0200

    Moved fluent api configuration to separate configuration file

---
+++ MyWebshop.ConsoleApp/DAL/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyWebshop.ConsoleApp.Models;
+using System.Reflection.Emit;
+
+namespace MyWebshop.ConsoleApp.DAL.Configurations;
+
+public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        builder
+            .HasKey(c => c.Id);
+
+        builder
+            .Property(c => c.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder
+            .Property(c => c.PhoneNumber)
+            .HasMaxLength(20)
+            .IsRequired(false);
+
+        builder
+            .Property(c => c.CreditLimit)
+            .HasColumnType("decimal(18,2)")
+            .HasPrecision(18, 2);     // 👈 niet echt meer nodig, puur voor andere databases ...
+
+        builder
+            .Property(c => c.CreatedAt)
+            .HasColumnType("datetime2(0)")
+            .HasDefaultValueSql("SYSDATETIME()")   // 👈 default constraint
+            .IsRequired();
+    }
+}
--- MyWebshop.ConsoleApp/DAL/WebShopDbContext.cs
+++ MyWebshop.ConsoleApp/DAL/WebShopDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MyWebshop.ConsoleApp.DAL.Configurations;
 using MyWebshop.ConsoleApp.Models;
 
 namespace MyWebshop.ConsoleApp.DAL;
@@ -20,29 +21,7 @@ public class WebShopDbContext : DbContext
     {
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<Customer>()
-            .HasKey(c => c.Id);
-
-        modelBuilder.Entity<Customer>()
-            .Property(c => c.Name)
-            .HasMaxLength(100)
-            .IsRequired();
-
-        modelBuilder.Entity<Customer>()
-            .Property(c => c.PhoneNumber)
-            .HasMaxLength(20)
-            .IsRequired(false);
-
-        modelBuilder.Entity<Customer>()
-            .Property(c => c.CreditLimit)
-            .HasColumnType("decimal(18,2)")
-            .HasPrecision(18, 2);     // 👈 niet echt meer nodig, puur voor andere databases ...
-
-        modelBuilder.Entity<Customer>()
-            .Property(c => c.CreatedAt)
-            .HasColumnType("datetime2(0)")
-            .HasDefaultValueSql("SYSDATETIME()")   // 👈 default constraint
-            .IsRequired();
+        modelBuilder.ApplyConfiguration<Customer>(new CustomerConfiguration());
 
     }
 }
