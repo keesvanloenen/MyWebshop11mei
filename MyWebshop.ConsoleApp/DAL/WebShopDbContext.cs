aaa8e14b646e1c93f9ab5701990eb703a7dcbdc5
Kees van Loenen <kees.vanloenen@infosupport.com>
2026-05-11 14:23:41 +0200

    Fluent API explanation

--- MyWebshop.ConsoleApp/DAL/WebShopDbContext.cs
+++ MyWebshop.ConsoleApp/DAL/WebShopDbContext.cs
@@ -15,4 +15,34 @@ public class WebShopDbContext : DbContext
     //{
     //    builder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MyWebshop;ConnectRetryCount=0");
     //}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Customer>()
+            .HasKey(c => c.Id);
+
+        modelBuilder.Entity<Customer>()
+            .Property(c => c.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        modelBuilder.Entity<Customer>()
+            .Property(c => c.PhoneNumber)
+            .HasMaxLength(20)
+            .IsRequired(false);
+
+        modelBuilder.Entity<Customer>()
+            .Property(c => c.CreditLimit)
+            .HasColumnType("decimal(18,2)")
+            .HasPrecision(18, 2);     // 👈 niet echt meer nodig, puur voor andere databases ...
+
+        modelBuilder.Entity<Customer>()
+            .Property(c => c.CreatedAt)
+            .HasColumnType("datetime2(0)")
+            .HasDefaultValueSql("SYSDATETIME()")   // 👈 default constraint
+            .IsRequired();
+
+    }
 }
--- MyWebshop.ConsoleApp/Models/Customer.cs
+++ MyWebshop.ConsoleApp/Models/Customer.cs
@@ -5,22 +5,13 @@ namespace MyWebshop.ConsoleApp.Models;
 
 public class Customer
 {
-    [Key]
     public int Id { get; set; }
 
-    //[Column(TypeName= "uniqueidentifier")]
-    //public Guid Id { get; set; } = Guid.NewGuid();
-
-    [Required]
-    [MaxLength(100)]
     public required string Name { get; set; }
 
-    [MaxLength(20)]
     public string? PhoneNumber { get; set; }
 
-    [Column(TypeName = "decimal(8,2)")]
     public decimal CreditLimit { get; set; }
 
-    [Column(TypeName = "datetime2(0)")]
     public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
